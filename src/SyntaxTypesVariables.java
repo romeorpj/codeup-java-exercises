c97b0a9262ea21e56d4a27743581ff12d6235a26
Richard Jackson <rpjackson2@gmail.com>
2022-04-06 13:53:24 -0500

    fined syntax exercise

---
+++ src/SyntaxTypesVariables.java
@@ -0,0 +1,74 @@
+public class SyntaxTypesVariables<x> {
+    public static void main(String[] args) {
+
+//        JS VARIABLE DECLARATION
+//        LET SOMETHING = VALUE
+
+
+
+//        JAVA VARIABLE DECLARATION
+//        DATATYPE NAME = VALUE
+
+        byte myByte = 127;
+        byte daysOfMonth = 31;
+
+        short employees = 10000;
+
+        int userId = 2000000;
+
+        long milkyWay = 30000000000000L;
+
+        float roundPi = 3.14f;
+        double someBigDecimal = 3.1415926537878;
+
+        char yesNo = 'Y';
+
+        boolean iLikePizza = true;
+
+//        String someQuote = "\"Some cliche quote\" - Mark Twain";
+//        System.out.println(someQuote);
+
+//        double pi = 3.14159;
+//        int almostPi = (int) pi;
+//        System.out.println(almostPi);
+
+        int myFavoriteNumber = 17;
+        System.out.println(myFavoriteNumber);
+
+        String myString = "These are my string\"s with an improper quotation for testing escape chars";
+//        myString = 'u';
+        double pi = 3.14159;
+
+        float myNumber = 3.14f;
+        System.out.println(myNumber);
+
+
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+
+//        String class = "a string";
+
+        String theNumberThree = "three";
+        Object o = theNumberThree;
+        int three = (int) o;
+        System.out.println(three);
+
+
+
+        int x = 4;
+        x += 5;
+
+
+      long longNumber =   Integer.MAX_VAL + 1;
+        System.out.println(longNumber);
+    }
+
+
+}
