c817a67c6489a88bdbbd6cf242b5a04dab4bdcc1
Richard Jackson <rpjackson2@gmail.com>
2022-04-08 09:49:32 -0500

    STRINGS

---
+++ src/Bob.java
@@ -0,0 +1,37 @@
+
+import java.util.Objects;
+import java.util.Scanner;
+
+public class Bob {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+        String continueWithBobby;
+        do {
+            System.out.println("Talk to Bobby... ");
+            String talkToBobby = sc.nextLine();
+
+            if (talkToBobby.endsWith("?")) {
+                System.out.println("Sure.");
+            } else if (talkToBobby.endsWith("!")) {
+                System.out.println("Whoa, chill out!");
+            } else if (talkToBobby.equals("")) {
+                System.out.println("Fine. Be that way!");
+            } else {
+                System.out.println("Whatever.");
+            }
+
+            System.out.println("would you like to continue talking toBobby? y/n:");
+            continueWithBobby = sc.nextLine();
+            if(Objects.equals(continueWithBobby, "n")){
+                break;
+            }else if(!continueWithBobby.equals("y") && !continueWithBobby.equals("n")){
+                System.out.println("You didn't follow instructions, bye!");
+                break;
+            }
+        } while (Objects.equals(continueWithBobby,"y"));
+
+
+    }
+}
--- src/StringExercise.java
+++ src/StringExercise.java
@@ -1,9 +1,8 @@
-import java.sql.SQLOutput;
-import java.util.Objects;
+
 import java.util.Scanner;
 public class StringExercise {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+
 
         String var1 = "We don't need no education";
         var1 = "We don't need no thought control";
